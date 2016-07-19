484dea3d6e4806aa51425bd5a413ca7cbbde71f8
北河 <itbeihe@gmail.com>
2016-07-19 15:03:14 +0800

    update

--- component/demos/demo/data.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $city = $_GET['city'];
-    $str =file_get_contents("http://v.juhe.cn/weather/index?cityname=".$city."&dtype=&format=&key=eb220d0001781be2639b7ee9c787e974");
-    echo $str;
-?>
